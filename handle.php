594b3d80268036e113cd00cb53399803b413d156
Peter Munene <68741159+munenepeter@users.noreply.github.com>
2022-03-30 18:08:42 +0300

    removed src

--- handle.php
+++ handle.php
@@ -1,30 +1,25 @@
 <?php
+include_once 'src/bootstrap.php';
 
 if (isset($_POST['btn'])) {
-    echo "<pre>";
-    print_r($_POST);
-    $case_no  = $_POST['btn'];
-    $date_of_delivery = $_POST['btn'];
-    $court  = $_POST['btn'];
-    $case_parties  = $_POST['btn'];
-    $case_doc  = $_POST['btn'];
-    $created_at  = $_POST['btn'];
-    $updated_at = $_POST['btn'];
+
+    $case_no  = $_POST['case_no'];
+    $date_of_delivery = $_POST['date_of_delivery'];
+    $court  = $_POST['court'];
+    $case_parties  = $_POST['case_parties'];
+    $case_doc  = $_POST['case_doc'];
+    $created_at  = date('Y-m-d H:i:s', time());;
+    $updated_at = date('Y-m-d H:i:s', time());;
 
 
-    $DB->query("
+
+    try {
+        $DB->query("
     INSERT INTO cases (case_no, date_of_delivery, court, case_parties, case_doc, created_at, updated_at )
-    VALUES ($case_no, $date_of_delivery, $court, $case_parties, $case_doc, $created_at, $updated_at);
+    VALUES ('$case_no', '$date_of_delivery', '$court', '$case_parties', '$case_doc', '$created_at', '$updated_at');
     ");
-
-    /*
-
-    case_no VARCHAR NOT NULL,
-    date_of_delivery DATETIME,
-    court VARCHAR,
-    case_parties TEXT,
-    case_doc VARCHAR,
-    created_at DATETIME,
-    updated_at DATETIME
-    */
+        header("Location:{$_SERVER['HTTP_REFERER']}");
+    } catch (\PDOException $e) {
+        echo $e->getMessage();
+    }
 }
--- index.php
+++ index.php
@@ -1,6 +1,3 @@
-<?php
-include_once 'src/bootstrap.php';
-?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -15,82 +12,53 @@ include_once 'src/bootstrap.php';
 
 <body class="bg-gray-100 px-3 font-sans leading-normal tracking-normal">
     <div class=" mt-4  max-w-screen-md max-h-screen mx-auto p-4 text-center">
-        <h2 class="font-bold text-xl">Upload Case Metadata</h2>
+        <h2 class="font-bold text-xl">Search a Case</h2>
     </div>
-    <div class="mx-auto p-5 w-1/2">
-        <!-- date of delivery & Court -->
-        <div class="p-4 bg-white border rounded-lg  ">
 
-            <form action="handle.php" method="post" class="bg-white container flex flex-col mx-auto">
-                <div class="mt-2">
-                    <label class="text-sm font-medium text-gray-900" for="case_no" class="text-sm">Case Number</label>
-                    <input id="case_no" name="case_no" type="text" placeholder="Case Number" class="p-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full">
+    <div class="h-full bg-gray-200 text-gray-800 p-4 lg:p-8" x-data="alpineInstance()" x-init="getData()">
+        <div class="flex flex-col max-w-screen-md max-h-screen mx-auto p-4">
+            <div class="relative">
+                <input x-model="search" class="w-full px-2 py-1 border focus:border-blue-300 ring-blue-300 focus:ring focus:outline-none" type="text" placeholder="Search..." />
+
+                <button x-show="search.length > 0" @click="search = ''" class="absolute right-0 top-0 w-6 h-full flex justify-center items-center focus:outline-none text-gray-700 focus:text-gray-900">
+                    <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
+                        </path>
+                    </svg>
+                </button>
+            </div> 
+           
+        </div>
+        <div class="flex flex-wrap -mx-2 pb-8">
+            <!-- begin: law card -->
+            <template x-for="law in laws" :key="law.case_id">
+                <div class="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 h-full font-light">
+                    <div class="flex bg-white rounded-lg shadow-md m-2 border-l-4 border-white hover:shadow-2xl hover:border-pink-500 cursor-pointer relative">
+                        <div class="p-4 pr-6 leading-normal">
+                            <div class="font-medium text-xl truncate" x-text="law.case_no"></div>
+                            <div class="truncate uppercase text-xs text-gray-500 font-semibold pb-2 tracking-widest" x-text="law.case_parties"></div>
+                            <div class="" x-text="law.date_of_delivery"></div>
+                            <a class="text-blue-600 hover:text-blue-700 mr-4 block" x-bind:href="'mailto:' + law.court" x-text="law.court"></a>
+                            <a class="text-blue-600 hover:text-blue-700 block" x-bind:href="'https://' + law.website" x-text="law.created_at"></a>
+                        </div>
+                    </div>
                 </div>
-                <div class="mt-2">
-                    <label class="text-sm font-medium text-gray-900" for="case_parties" class="text-sm">Case Parties</label>
-                    <input id="case_parties" name="case_parties" type="text" placeholder="Case Parties" class="p-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full">
-                </div>
-                <div class="mt-2">
-                    <label class="text-sm font-medium text-gray-900" for="date_of_delivery" class="text-sm">Date of Delivery</label>
-                    <input id="date_of_delivery" name="date_of_delivery" type="date" placeholder="Date of Delivery" class="p-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full">
-                </div>
-                <div class="mt-2">
-                    <label class="text-sm font-medium text-gray-900" for="court" class="text-sm">Court</label>
-                    <input id="court" name="court" type="text" placeholder="Court" class="p-3 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full">
-                </div>
-                <div class="mt-2">
-                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300" for="user_avatar">Upload file</label>
-                    <input name="case_doc" class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer"  type="file">
-                    <!-- <div class="mt-1 text-sm text-gray-500 dark:text-gray-300" id="user_avatar_help">A profile picture is useful to confirm your are logged into your account</div> -->
-                </div>
-
-
-                <div class="col-span-full sm:col-span-3 mt-4">
-                    <input type="submit" name="btn" class="bg-blue-500 text-white text-sm font-medium px-6 py-2 rounded capitalize cursor-pointer" value="Upload Metadata">
-                    <br> <a class="ml-2 text-blue-600 hover:underline hover:text-blue-700" href="javascript:history.back()">Go back</a>
-                </div>
-
-
-
-
-            </form>
+            </template>
+            <!-- end: law card -->
         </div>
     </div>
-    </div>
-
-    <!-- <div x-data="lawSearch()" class="flex flex-col max-w-screen-md max-h-screen mx-auto p-4">
-        <div class="relative">
-            <input x-model="search" class="w-full px-2 py-1 border focus:border-blue-300 ring-blue-300 focus:ring focus:outline-none" type="text" placeholder="Search..." />
-
-            <button x-show="search.length > 0" @click="search = ''" class="absolute right-0 top-0 w-6 h-full flex justify-center items-center focus:outline-none text-gray-700 focus:text-gray-900">
-                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
-                    </path>
-                </svg>
-            </button>
-        </div> -->
-
-
-    <ul class="mt-4 overflow-auto">
-        <template x-for="law in filteredLaws()" :key="law">
-            <li x-html="highlightSearch(law)"></li>
-        </template>
-    </ul>
-    </div>
     <script>
-        function lawSearch() {
+        function alpineInstance() {
             return {
-                laws: [
-                    "The Interpretation and General Provisions Act",
-                    "The Law Reform Commission Act",
-                    "The Constitution of Kenya Review Act (Expired)",
-                    "The Architects and Quantity Surveyors Act",
-                    "The Engineers Registration Act",
-                    "The Certified Public Secretaries of Kenya Act",
-                    "The Capital Markets Authority Act"
-                ],
-
+                laws: [],
                 search: '',
+                getData() {
+                    fetch('http://localhost:3000/src/api.php')
+                        .then(response => response.json())
+                        .then((data) => {
+                            this.laws = data; 
+                        })
+                },
                 filteredLaws() {
                     return this.laws.filter(
                         law => law.toLowerCase().includes(this.search.toLowerCase())
@@ -107,6 +75,91 @@ include_once 'src/bootstrap.php';
             }
         }
     </script>
+
+    <!-- <div x-data="lawSearch()" x-init="getData()" class="flex flex-col max-w-screen-md max-h-screen mx-auto p-4">
+        <div class="relative">
+            <input x-model="search" class="w-full px-2 py-1 border focus:border-blue-300 ring-blue-300 focus:ring focus:outline-none" type="text" placeholder="Search..." />
+
+            <button x-show="search.length > 0" @click="search = ''" class="absolute right-0 top-0 w-6 h-full flex justify-center items-center focus:outline-none text-gray-700 focus:text-gray-900">
+                <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12">
+                    </path>
+                </svg>
+            </button>
+        </div>
+       
+
+        <ul class="mt-4 overflow-auto">
+            <template x-for="law in filteredLaws()" :key="law">
+                <li x-html="highlightSearch(law)"></li>
+            </template>
+        </ul>
+    </div>
+    <script>
+        // function getLaws() {
+
+        //     let laws = {};
+        //     fetch('http://localhost:3000/src/api.php', {
+        //             headers: {
+        //                 'Content-Type': 'application/json',
+        //                 'Accept': 'application/json'
+        //             }
+        //         })
+        //         .then(res => res.json())
+        //         .then((data) => {
+        //             laws = data;
+        //            // console.log(data)
+        //         });
+
+        //     console.log(laws);
+        // }
+        // getLaws();
+
+        function lawSearch(laws) {
+            return {
+                laws: [],
+                teams: null,
+                search: '',
+
+                getData() {
+                    fetch('http://localhost:3000/src/api.php')
+                        .then(response => response.json())
+                        .then(response => {
+                            this.teams = response;
+                            isLoading = false;
+                            console.log(response);
+                        })
+                        console.log(this.teams);
+                    // fetch('http://localhost:3000/src/api.php', {
+                    //         headers: {
+                    //             'Content-Type': 'application/json',
+                    //             'Accept': 'application/json'
+                    //         }
+                    //     })
+                    //     .then(res => res.json())
+                    //     .then((data) => {
+                    //         console.log(this.laws)
+                    //         this.laws = data;
+                    //         console.log(this.laws)
+                    //         console.log(data)
+                    //     });
+                },
+                filteredLaws() {
+                    return this.laws.filter(
+                        law => law.toLowerCase().includes(this.search.toLowerCase())
+                    );
+                },
+                highlightSearch(s) {
+                    if (this.search === '') return s;
+
+                    return s.replaceAll(
+                        new RegExp(`(${this.search.toLowerCase()})`, 'ig'),
+                        '<strong class="font-semibold bg-blue-200">$1</strong>'
+                    )
+                }
+            }
+        }
+    </script> -->
 </body>
 
 </html>
