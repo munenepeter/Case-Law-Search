e4b84a3bee69dd22d25a42ed04a20563581b2219
Peter Munene <peternjega7@gmail.com>
2024-12-02 16:37:15 +0300

    feat: add cli support for scrapping

---
+++ routes/console.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Console\Commands\ScrapeGazettes;
+
+return [
+    'scrape:gazettes' => ScrapeGazettes::class,
+    // Add more commands here
+    // 'command:name' => CommandClass::class,
+]; 
